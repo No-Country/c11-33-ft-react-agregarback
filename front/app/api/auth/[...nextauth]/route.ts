cace3ae8a1ef242272f41f6efdd16d4afe5e3c22
Dreyz <54697159+JuanQuiro@users.noreply.github.com>
2023-05-20 18:38:06 +0000

    fix: :bug: error con las variables de entorno en los provedores de next auth

--- front/app/api/auth/[...nextauth]/route.ts
+++ front/app/api/auth/[...nextauth]/route.ts
@@ -13,8 +13,8 @@ export const authOptions: NextAuthOptions = {
       clientSecret: process.env.GOOGLE_CLIENT_SECRET as string,
     }),
     FacebookProvider({
-      clientId: process.env.GOOGLE_CLIENT_ID as string,
-      clientSecret: process.env.GOOGLE_CLIENT_SECRET as string,
+      clientId: process.env.FACEBOOK_CLIENT_ID as string,
+      clientSecret: process.env.FACEBOOK_CLIENT_SECRET as string,
     }),
   ],
 };
