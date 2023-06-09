cc7c2056693775a734ebe976f3c369a15ea519db
Dreyz <54697159+JuanQuiro@users.noreply.github.com>
2023-06-09 18:54:52 +0000

    feat: :sparkles: añadido estilos a mesaje de auth y introduccion de funcion aut. de cuentas

--- components/auth/NotLoggedInMessage.tsx
+++ components/auth/NotLoggedInMessage.tsx
@@ -1,19 +1,32 @@
+import Image from "next/image";
 import Link from "next/link";
 import React from "react";
+import img from "../../public/assets/no-auth.gif";
 
 const NotLoggedInMessage: React.FC = () => {
   return (
-    <div className="z-50 rounded-lg border-gray-300 p-4">
-      <p className="text-lg font-medium">You are not logged in</p>
+    <div className="z-50 min-h-screen items-center justify-center rounded-lg border-gray-300 p-4">
+      <Image
+        src={img}
+        width={500}
+        height={500}
+        alt="Not auth"
+        className="my-3 rounded-md"
+        placeholder="blur"
+      />
+      <p className="text-center text-lg font-medium text-white">
+        You are not logged in
+      </p>
       <div className="mt-4 flex justify-between">
-        <Link href={"/api/auth/signin"}>
-          <button className="rounded bg-blue-500 px-4 py-2 text-white hover:bg-blue-600">
-            Login
-          </button>
-        </Link>
         <Link href={"/"}>
           <button className="ml-4 rounded bg-gray-200 px-4 py-2 text-gray-700 hover:bg-gray-300">
             Home
+          </button>
+        </Link>
+
+        <Link href={"/api/auth/signin"}>
+          <button className="rounded bg-black/70 px-4 py-2 text-white hover:bg-black hover:text-white">
+            Login
           </button>
         </Link>
       </div>
