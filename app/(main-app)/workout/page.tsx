7612400fdc7af3213da5d92cc9603f5291ff0e7c
Dreyz <54697159+JuanQuiro@users.noreply.github.com>
2023-06-09 20:13:36 +0000

    style: :lipstick: añadido estilos en workout

--- app/(main-app)/workout/page.tsx
+++ app/(main-app)/workout/page.tsx
@@ -25,49 +25,51 @@ export default async function Home() {
           width={300}
           height={300}
         />
-        <div className="flex items-center justify-center">
-          <Link
-            href={"/exercises"}
-            className="boder-2 font-bolder mt-3 justify-center rounded-3xl border-black bg-accent-600 px-5 py-2 text-center text-xs text-primary-500 duration-300 hover:border-green-600 hover:bg-gray-700 hover:text-green-300 lg:text-base"
-          >
-            START AN EMPTY WORKOUT
-          </Link>
-        </div>
-        <div className="flex items-center justify-center">
-          <Link
-            href={"/workout/item-workout"}
-            className="boder-2 font-bolder mt-3 justify-center rounded-3xl border-black bg-accent-500 px-5 py-2 text-center text-xs text-neutral-100 duration-300 hover:border-green-600 hover:bg-gray-700 hover:text-green-300 lg:text-base"
-          >
-            WORKOUT IN CURSE
-          </Link>
-        </div>
-        <div className="mt-9 grid grid-cols-1 gap-9 sm:gap-0 2xl:grid-cols-3">
-          <div>
-            <div className="mb-3">
-              <h2 className="mr-2 inline uppercase text-black/80">
-                MY TEMPLATES
-              </h2>
-              <span className="text-2xl text-black/90">+</span>
-            </div>
-            <div className="font-roboto w-40 text-xs">
-              <p>You don’t have any custom templates yet.</p>
-              <br />
-              <p>Tap the ‘+’ button to create your first template!</p>
-            </div>
+        <div className="text-white/70">
+          <div className="flex items-center justify-center ">
+            <Link
+              href={"/exercises"}
+              className="boder-2 font-bolder mt-3 justify-center rounded-3xl border-black bg-primary-500/50 px-5 py-2 text-center text-xs text-white duration-300 hover:border-green-600 hover:bg-gray-700 hover:text-green-300 lg:text-base"
+            >
+              START AN EMPTY WORKOUT
+            </Link>
           </div>
-
-          <div className="font-roboto sm:col-start-3">
-            <div className="mb-3">
-              <h2 className="mr-2 inline uppercase text-black/80">
-                SAMPLE TEMPLATES
-              </h2>
-              <span className="text-2xl text-black/90">+</span>
+          <div className="flex items-center justify-center">
+            <Link
+              href={"/workout/item-workout"}
+              className="boder-2 font-bolder mt-3 justify-center rounded-3xl border-black bg-primary-500  px-5 py-2 text-center text-xs text-neutral-100 duration-300 hover:border-green-600 hover:bg-gray-700 hover:text-green-300 lg:text-base"
+            >
+              WORKOUT IN CURSE
+            </Link>
+          </div>
+          <div className="mt-9 grid grid-cols-1 gap-9 sm:gap-0 2xl:grid-cols-3">
+            <div>
+              <div className="mb-3">
+                <h2 className="mr-2 inline font-bold uppercase text-white/70">
+                  MY TEMPLATES
+                </h2>
+                <span className="text-2xl text-white">+</span>
+              </div>
+              <div className="font-roboto w-40 text-xs">
+                <p>You don’t have any custom templates yet.</p>
+                <br />
+                <p>Tap the ‘+’ button to create your first template!</p>
+              </div>
             </div>
-            <div className="rounded-md border border-black p-2">
-              <p className="w-40 text-sm font-semibold">Strong 5x5</p>
-              <p className="text-xs">5 x Squat (Barbell)</p>
-              <p className="text-xs">5 x Bench press (Barbell)</p>
-              <p className="text-xs">5 x Bent Over Row (Barbell)</p>
+
+            <div className="font-roboto sm:col-start-3">
+              <div className="mb-3">
+                <h2 className="mr-2 inline font-bold uppercase text-white/70">
+                  SAMPLE TEMPLATES
+                </h2>
+                <span className="text-2xl text-white">+</span>
+              </div>
+              <div className="rounded-md border border-black p-2">
+                <p className="w-40 text-sm font-semibold">Strong 5x5</p>
+                <p className="text-xs">5 x Squat (Barbell)</p>
+                <p className="text-xs">5 x Bench press (Barbell)</p>
+                <p className="text-xs">5 x Bent Over Row (Barbell)</p>
+              </div>
             </div>
           </div>
         </div>
